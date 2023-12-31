64af3dd71f9bef319664f43fc978d061918302a2
oyoops <121578862+oyoops@users.noreply.github.com>
2023-12-31 16:58:19 -0500

    dd

--- src/DApp_nav.js
+++ src/DApp_nav.js
@@ -4,19 +4,19 @@ import Currency from "./images/currency.svg";
 import Claim from "./images/claim-coin.svg";
 import { Link, NavLink } from "react-router-dom";
 
-const NAME_FOR_STAKING = `Stake $ROCKWIFHAT`;
-const NAME_FOR_BURNING = `Burn $ROCKWIFHAT`;
-const NAME_FOR_CLAIMING = `Claim $ROCKWIFHAT`;
+const NAME_FOR_STAKING = `Stake`;
+const NAME_FOR_BURNING = `Burn`;
+const NAME_FOR_CLAIMING = `Claim`;
 
 const APP_DESC_P1 = `RockWifHat dApp is UNDER CONSTRUCTION! 👷🏼‍♂️`;
-const APP_DESC_P2 = `Please pardon my rock dust. The Phantom/MetaMask wallet connections should work, but there are NO smart contracts tied to these buttons.`;
+const APP_DESC_P2 = `The Phantom/MetaMask wallet connections should work, but there are NO smart contracts tied to these buttons.`;
 
 
 export function DAppNav() {
     return (
       <div className="ConnectWallet card_dapp_nav">
         
-        <h3>Apps</h3>
+        <h3>Menu</h3>
         
         <NavLink to="../dApp" activeClassName="btn-top-burn">
           <button className="btn btn--accent2 text--small text--medium btn-top-burn" >
