13010d617d42bdb92a389c5588d07d2ac6f48826
oyoops <121578862+oyoops@users.noreply.github.com>
2023-12-31 16:00:08 -0500

    gfd

--- src/Roadmap.js
+++ src/Roadmap.js
@@ -3,29 +3,29 @@ import React from "react";
 
 
 // ROADMAP:
-const ROADMAP_SECTION_TITLE = `RoadmapWifHat`;
-const ROADMAP_SECTION_INTRODUCTION = `Charting $ROCKWIFHAT's journey through the Solana jeetcosystem:`;
+const ROADMAP_SECTION_TITLE = `Roadmap`;
+const ROADMAP_SECTION_INTRODUCTION = `Charting RockWifHat's journey forward, tentatively:`;
 
 // GREEN:
-const ROADMAP_GREEN_TITLE = `Now Rocking & Rolling`;
-const ROADMAP_GREEN_ITEM_1 = `Launch of $ROCKWIFHAT Token (TODAY!)`;
-const ROADMAP_GREEN_ITEM_2 = `AI-Powered Meme Bot! (From Scratch + Trained Model = BASED DEV.)`;
-const ROADMAP_GREEN_ITEM_3 = `Enhanced DEXScreener Profile`;
-const ROADMAP_GREEN_ITEM_4 = `Launch of New Chic Social Media Channels and a Stylish Website`;
+const ROADMAP_GREEN_TITLE = `Rocking & Rolling`;
+const ROADMAP_GREEN_ITEM_1 = `Launch of $ROCKWIFHAT`;
+const ROADMAP_GREEN_ITEM_2 = `AI-Powered Meme Bot`;
+const ROADMAP_GREEN_ITEM_3 = `Enhanced DEXScreener`;
+const ROADMAP_GREEN_ITEM_4 = `Social Media`;
 
 // YELLOW:
-const ROADMAP_YELLOW_TITLE = `Just Around the Top Hat`;
-const ROADMAP_YELLOW_ITEM_1 = `Creation of 'Chic Pebbles' NFTs by Renowned Artist Bok`;
-const ROADMAP_YELLOW_ITEM_2 = `Achieving Listings on CMC & CoinGecko (Harder!)`;
-const ROADMAP_YELLOW_ITEM_3 = `Hosting 'Top Hat Tuesday' (Community Contests & Giveaways)`;
-const ROADMAP_YELLOW_ITEM_4 = `BirdEye Enhanced Info and Strategic Partnerships`;
+const ROADMAP_YELLOW_TITLE = `Just Around the Hat`;
+const ROADMAP_YELLOW_ITEM_1 = `NFTs by In-House NFT Artist`;
+const ROADMAP_YELLOW_ITEM_2 = `CMC & CoinGecko Listings`;
+const ROADMAP_YELLOW_ITEM_3 = `'Top Hat Tuesday' Giveaways`;
+const ROADMAP_YELLOW_ITEM_4 = `BirdEye/Dextools Enhanced Info`;
 
 // RED:
-const ROADMAP_RED_TITLE = `A Stone's Throw Away`;
-const ROADMAP_RED_ITEM_1 = `Airdrop 'Chic Pebbles' NFTs to $ROCKWIFHAT Holders`;
-const ROADMAP_RED_ITEM_2 = `Expand Token Utility via AI-Powered Social Features`;
-const ROADMAP_RED_ITEM_3 = `List on a centralized exchange!`;
-const ROADMAP_RED_ITEM_4 = `Merch! Top Hats and Pet Rocks`;
+const ROADMAP_RED_TITLE = `Stone's Throw Away`;
+const ROADMAP_RED_ITEM_1 = `NFT Airdrop to Holders`;
+const ROADMAP_RED_ITEM_2 = `AI Social Utility 2.0 (Big)`;
+const ROADMAP_RED_ITEM_3 = `CEX Listing`;
+const ROADMAP_RED_ITEM_4 = `Top Hat & Pet Rock Merch`;
 
 
 class Roadmap extends React.Component {
